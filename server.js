93e2ebc711f724e01848a46050893f24f3bf07c4
Brandon <blshort43@gmail.com>
2016-12-10 14:39:26 -0600

    mongo key

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 // Database configuration with mongoose
-mongoose.connect("MONGO_KEY");
+mongoose.connect("mongodb://localhost/week18hw");
+
+mongoose.connect(process.argv.MONGO_KEY || "mongodb://localhost/week18hw");
 var db = mongoose.connection;
 
 // Show mongoose errors
